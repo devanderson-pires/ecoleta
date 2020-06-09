0567be9b965381e74a88e82aa8491b4c4c3faeaf
anderson <devanderson.pires@gmail.com>
2020-06-09 12:49:01 -0300

    Funcionalidades avançadas

--- server/src/controllers/PointsControllers.ts
+++ server/src/controllers/PointsControllers.ts
@@ -18,8 +18,16 @@ class PointsController {
       .where('uf', String(uf))
       .distinct()
       .select('points.*');
+    
+    const serializedPoints = points.map(point =>{
 
-    return res.json(points);
+      return {
+        ...point,
+        image_url: `http://192.168.0.110:3333/uploads/${point.image}`
+      };
+    });
+
+    return res.json(serializedPoints);
   };
 
   async show(req: Request, res: Response) {
@@ -30,12 +38,17 @@ class PointsController {
 
     if(!point) return res.status(400).json({ message: 'Point not found.' });
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.0.110:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return res.json({ point, items });
+    return res.json({ point: serializedPoint, items });
   };
 
   async create(req: Request, res: Response) {
@@ -56,7 +69,7 @@ class PointsController {
 
     const point = {
       
-      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
@@ -70,14 +83,17 @@ class PointsController {
   
     const point_id = insertIds[0];
   
-    const pointItems = items.map((item_id: number) => {
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
   
-      return {
-  
-        item_id,
-        point_id,
-      };
-    });
+        return {
+    
+          item_id,
+          point_id,
+        };
+      });
   
     await trx('point_items').insert(pointItems);
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsControllers';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -11,7 +16,23 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 // Cadastro de ponto de coleta
-routes.post('/points', pointsController.create);
+routes.post('/points', 
+upload.single('image'), 
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    uf: Joi.string().required().max(2),
+    city: Joi.string().required(),
+    items: Joi.string().required(),
+  })
+},{
+  abortEarly: false
+}),
+pointsController.create);
 
 // lista um ponto de coleta específico
 routes.get('/points/:id', pointsController.show);
